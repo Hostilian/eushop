ce79cee17eb2b56d56d4def6c3ff4f65a369514c
Eren Ozturk <XOZTE001@studenti.czu.cz>
2026-07-07 20:33:08 +0200

    l

--- services/core-service/src/main/java/com/eushop/core/entity/ConsentLog.java
+++ services/core-service/src/main/java/com/eushop/core/entity/ConsentLog.java
@@ -3,6 +3,8 @@ package com.eushop.core.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
