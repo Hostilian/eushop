1f6c6085d6fad06f3ee67c96670f8e81366b5c65
Eren Ozturk <XOZTE001@studenti.czu.cz>
2026-07-21 01:38:09 +0200

    Update [id].tsx

--- apps/web/pages/products/[id].tsx
+++ apps/web/pages/products/[id].tsx
@@ -1,17 +1,3 @@
-import { Product } from "../../types/src/product";
+import FoodDetailPage from '../food/[id]';
 
-function ProductCard({ product }: { product: Product }) {
-    return (
-        <div>
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-            {product.gpsr && (
-                <div>
-                    <p>Manufacturer: {product.gpsr.manufacturerName}</p>
-                    <p>Address: {product.gpsr.manufacturerAddress}</p>
-                    <p>Safety Contact: {product.gpsr.safetyContact}</p>
-                </div>
-            )}
-        </div>
-    );
-)
+export default FoodDetailPage;
